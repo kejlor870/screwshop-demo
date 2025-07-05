9c02153065556463ed3315db1a5c508dc3fed168
kejlor870 <71232096+kejlor870@users.noreply.github.com>
2025-07-05 22:08:47 +0200

    initial commit

--- scripts.js
+++ scripts.js
@@ -69,6 +69,9 @@ function dragOvr(event){
 
 }
 
+// ------------
+// Functionality when cartboard is droped to shop cart section
+// ------------
 function drop(event){
     event.preventDefault();
 
